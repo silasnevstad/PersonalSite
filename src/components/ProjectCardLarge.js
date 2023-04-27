ba0161ea9ab69f1b00a356b68251136887c75363
Silas Nevstad <nevstads@gmail.com>
2023-04-26 23:25:00 -0400

    New Visist project button

--- src/components/ProjectCardLarge.js
+++ src/components/ProjectCardLarge.js
@@ -55,12 +55,56 @@ const ProjectDeveloper = styled.p`
     color: ${({ theme }) => theme.colors.text};
 `;
 
-const ProjectLink = styled.a`
+const ProjectLink = styled.button`
+    width: 10em;
+    text-align: left;
+    border: none;
+    background: none;
     font-size: 1.2rem;
     margin: 0;
     margin-top: 25px;
     opacity: 0.6;
     color: ${({ theme }) => theme.colors.secondary};
+    padding-bottom: 7px;
+    letter-spacing: 2px;
+    padding-right: 15px;
+    position: relative;
+    padding-bottom: 20px;
+
+    &:after {
+        content: "";
+        position: absolute;
+        width: 100%;
+        transform: scaleX(0);
+        height: 2px;
+        bottom: 0;
+        left: 0;
+        background-color: #000000;
+        transform-origin: bottom right;
+        transition: transform 0.25s ease-out;
+    }
+
+    &:hover:after {
+        transform: scaleX(1);
+        transform-origin: bottom left;
+    }
+
+    &:hover svg {
+        transform: translateX(15px);
+    }
+`;
+
+const ProjectLinkSVG = styled.svg`
+    transform: translateX(10px);
+    transition: all 0.3s ease;
+
+    &:active {
+        transform: scale(0.9);
+    }
+
+    &:hover {
+        transform: translateX(15px);
+    }
 `;
 
 const ProjectCardLarge = ({ project }) => {
@@ -76,7 +120,13 @@ const ProjectCardLarge = ({ project }) => {
             <ProjectDeveloper>{project.developers}</ProjectDeveloper>
             <ProjectDescription>{project.description}</ProjectDescription>
 
-            {project.url !== '' && <ProjectLink href={project.url} target="_blank">View Project</ProjectLink>}
+            {project.url !== '' && 
+            <ProjectLink href={project.url} target="_blank">
+                View Project
+                <ProjectLinkSVG viewBox="0 0 46 16" height="10" width="30" xmlns="http://www.w3.org/2000/svg" id="arrow-horizontal">
+                    <path transform="translate(30)" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" data-name="Path 10" id="Path_10"></path>
+                </ProjectLinkSVG>
+            </ProjectLink>}
             {/* You can add more project details here, like an image or technologies used */}
         </CardLarge>
     );
