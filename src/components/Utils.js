7cda7c1ee483c7693b2c0fe99833bfbcda8e0b86
Silas Nevstad <nevstads@gmail.com>
2023-05-04 20:59:39 -0400

    Updated project requests

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -4,10 +4,16 @@ const formatRequests = (project) => {
         return project.version;
     }
 
-    // if there are more than 1000 requests, format it to 1.2k
-    if (project.requests > 1000) {
+    // if there are more than 10000 requests, format it to 10.1k
+    if (project.requests > 10000) {
         return (project.requests / 1000).toFixed(1) + 'k requests';
     }
+
+    // if there are more than 1000 requests, format it to 1.23k
+    if (project.requests > 1000) {
+        return (project.requests / 1000).toFixed(2) + 'k requests';
+    }
+        
     return project.requests + ' requests';
 };
 
