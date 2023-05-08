e76dda0f2a522cca2aa64e0170a12766d16a5c77
Silas Nevstad <nevstads@gmail.com>
2023-05-08 11:05:43 -0400

    Stats manual update

--- src/components/Home.js
+++ src/components/Home.js
@@ -90,7 +90,7 @@ const Home = ({ isMenuOpen, toggleMenu }) => {
           <HomeContainer isMenuOpen={isMenuOpen}>
             <HomeText>{text}<span className="cursor">_</span></HomeText>
           </HomeContainer>
-          <FooterText>2.5k requests © 2021 Silas Nevstad</FooterText>
+          <FooterText>2.6k requests © 2021 Silas Nevstad</FooterText>
           <SocialLinks />
         </div>
     );
