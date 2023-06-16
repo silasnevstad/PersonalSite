636f039edb19b0a43bb872c53b5a204dea677722
Silas Nevstad <nevstads@gmail.com>
2023-06-15 20:03:14 -0400

    Stat updates

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -139,43 +139,6 @@ const FooterText = styled.p`
   }
 `;
 
-// OLD: Typewriter effect
-// const useTypewriterEffect = (text, initialDelay = 0) => {
-//   const [typedText, setTypedText] = useState('');
-//   const [isDeleting, setIsDeleting] = useState(false);
-//   const [typingDelay, setTypingDelay] = useState(0);
-
-//   useEffect(() => {
-//     const timeout = setTimeout(() => {
-//       const fullText = text;
-
-//       if (isDeleting) {
-//         setTypingDelay(1); // Faster deleting speed
-//         setTypedText(fullText.substring(0, typedText.length - 1));
-//       } else {
-//         // Random typing speed between 50ms to 150ms
-//         setTypingDelay(Math.random() * 40);
-//         setTypedText(fullText.substring(0, typedText.length + 1));
-//       }
-
-//       if (!isDeleting && typedText === fullText) {
-//         setTimeout(() => setIsDeleting(true), 3000);
-//         setTypingDelay(500);
-//       } else if (isDeleting && typedText === '') {
-//         setIsDeleting(false);
-//         setTypingDelay(500);
-//       }
-//     }, typingDelay);
-
-//     return () => clearTimeout(timeout);
-//   }, [isDeleting, text, typingDelay, typedText]);
-
-//   return typedText;
-// };
-// const introText = 
-//     "Welcome to my website! I'm Silas Nevstad, a student at Northeastern University studying Computer Science. Feel free to explore the different pages on my site and don't hesitate to reach out to me if you have any questions or just want to say hi.";
-// const text = useTypewriterEffect(introText, 15);
-
 const Home = ({ isMenuOpen, toggleMenu }) => {
   const sortedByRequests = [...PROJECTS].sort((a, b) => b.requests - a.requests);
   // use sortByDate() function from utils.js
@@ -201,7 +164,6 @@ const Home = ({ isMenuOpen, toggleMenu }) => {
         <NavigationHeader toggleMenu={toggleMenu} currentPage={"/"} />
         <HomeCenterContainer>
           <HomeContainer>
-            {/* <HomeText>{text}<span className="cursor">_</span></HomeText> */}
             <HomeTextSmall>Hi, I'm</HomeTextSmall>
             <HomeTextLarge style={{marginTop: '-.1em'}}>Silas Nevstad</HomeTextLarge>
             <HomeTextSmall style={{marginTop: '-.9em'}}>Northeastern Student | Programmer</HomeTextSmall>
@@ -230,7 +192,7 @@ const Home = ({ isMenuOpen, toggleMenu }) => {
           </HomeContainer>
           {selectedProject && <ProjectModal project={selectedProject} onClose={handleCloseModal} />}
         </HomeCenterContainer>
-        <FooterText>10.8k requests © 2022 Silas Nevstad</FooterText>
+        <FooterText>11.1k requests © 2022 Silas Nevstad</FooterText>
         <SocialLinks />
       </HomeContainer>
     </AppContainer>
