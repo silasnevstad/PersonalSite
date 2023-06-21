3f22f3279ca1306c3728d0898c9d96a4523f4f77
Silas Nevstad <nevstads@gmail.com>
2023-06-21 16:50:24 -0400

    stat update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -192,7 +192,7 @@ const Home = ({ isMenuOpen, toggleMenu }) => {
           </HomeContainer>
           {selectedProject && <ProjectModal project={selectedProject} onClose={handleCloseModal} />}
         </HomeCenterContainer>
-        <FooterText>11.7k requests © 2022 Silas Nevstad</FooterText>
+        <FooterText>11.8k requests © 2022 Silas Nevstad</FooterText>
         <SocialLinks />
       </HomeContainer>
     </AppContainer>
