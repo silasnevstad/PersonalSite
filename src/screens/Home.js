f6c361cc38461f5354ceca0f804d7a7902e47c8a
Silas Nevstad <nevstads@gmail.com>
2023-07-01 16:47:38 -0400

    stat update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -300,7 +300,7 @@ const Home = ({ isMenuOpen, toggleMenu }) => {
           {/* // <QuoteAuthor style={{animationDelay: '0.4s'}}>- {selectedQuote.author}</QuoteAuthor> */}
           {selectedProject && <ProjectModal project={selectedProject} onClose={handleCloseModal} />}
         </HomeCenterContainer>
-        <FooterText>13.4k requests{window.innerWidth > 768 && " © 2022 Silas Nevstad"}</FooterText>
+        <FooterText>13.9k requests{window.innerWidth > 768 && " © 2022 Silas Nevstad"}</FooterText>
         <SocialLinks />
       </HomeContainer>
     </AppContainer>
